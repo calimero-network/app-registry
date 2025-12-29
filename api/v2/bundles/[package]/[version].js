b5d646252e881c87ca2aff19239b63a5eb72b139
xilosada <xabi@calimero.network>
2025-12-29 07:47:52 +0100

    Revert "fix: add OPTIONS handling for CORS preflight in Vercel serverless functions"
    This reverts commit 387dfd3ad4f5afc4e074faf9b83337dfb052231c.

--- api/v2/bundles/[package]/[version].js
+++ api/v2/bundles/[package]/[version].js
@@ -19,14 +19,6 @@ function getStorage() {
 }
 
 module.exports = async (req, res) => {
-  // Handle CORS preflight requests
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    return res.status(200).end();
-  }
-
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
